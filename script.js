329e682281971fcb4df66139e1ea1b65228f63e9
AntonyDemchenko <demchenkoantony@gmail.com>
2022-01-26 17:00:20 +0200

    implemented all needed functional, added possibility delete all completed tasks, added work with localstorage.

--- script.js
+++ script.js
@@ -1,217 +1,277 @@
-"use strict"
+"use strict";
 
-let newTodo = document.querySelector('.todo__input');
-let todosList = document.querySelector('.todos-list');
-let storage = JSON.parse(localStorage.getItem('todos-list')) || [];
-let todoCount = document.querySelector('.todo-count__number');
-let tasksSection = document.querySelector('.tasks');
-let footer = document.querySelector('.footer');
-let checkAllBtn = document.querySelector('.check-all');
+let newTodo = document.querySelector(".todo__input");
+let todosList = document.querySelector(".todos-list");
 
+let todoCount = document.querySelector(".todo-count__number");
+let tasksSection = document.querySelector(".tasks");
+let footer = document.querySelector(".footer");
+let checkAllBtn = document.querySelector(".check-all");
 
 // CREATE NEW ELEMENT
-function createNewElement () {
-        let listItem = document.createElement('li');
-        listItem.className = "todos__item";
+function createNewElement(elementTitle, elementID, elementChecked) {
+  let listItem = document.createElement("li");
+  listItem.className = "todos__item";
+  listItem.setAttribute("item-id", elementID);
 
-        let listItemCheckbox = document.createElement('input');
-        listItemCheckbox.className = "todos__toggle";
-        listItemCheckbox.setAttribute("type", "checkbox");
-        listItem.append(listItemCheckbox)
+  let listItemCheckbox = document.createElement("input");
+  listItemCheckbox.className = "todos__toggle";
+  listItemCheckbox.setAttribute("type", "checkbox");
+  listItemCheckbox.checked = elementChecked;
+  listItem.append(listItemCheckbox);
 
-        let listItemTitle = document.createElement('p');
-        listItemTitle.className = "todos__title";
-        listItemTitle.textContent = newTodo.value;
-        listItem.append(listItemTitle);
+  let listItemTitle = document.createElement("p");
+  listItemTitle.className = "todos__title";
+  listItemTitle.textContent = elementTitle;
+  listItem.append(listItemTitle);
 
-        let listItemDelete = document.createElement('button');
-        listItemDelete.className = "todos__delete";
-        listItemDelete.innerHTML = '&#215;'
-        listItem.append(listItemDelete);
+  let listItemDelete = document.createElement("button");
+  listItemDelete.className = "todos__delete";
 
-        todosList.append(listItem)
+  listItemDelete.innerHTML = "&#215;";
+  listItem.append(listItemDelete);
+
+  todosList.append(listItem);
 }
 
+// RENDER ALL ELEMENTS FROM LOCALSTORAGE BY FIRST LOAD
+function firstLoadRender() {
+  let storage = JSON.parse(localStorage.getItem("todos-list")) || [];
 
-// SORT TODOS DEPENDING SORT TYPE
-function sortTasks(sortType, callback) {
-        if(sortType == 'active') {
-                sortType = false
-        } else if(sortType == 'completed') {
-                sortType = true
-        }
-                
-        let allTasks = document.querySelectorAll('.todos__item')
-        let activeTasks = [];
-        allTasks.forEach(element => {
-        if(element.querySelector('.todos__toggle').checked === sortType) {
-                // activeTasks.push(element) 
-                callback(element, activeTasks)
-                }
-        });
-        
-        return activeTasks
+  storage.forEach((item) =>
+    createNewElement(item.title, item.id, item.completed)
+  );
+  if (storage.length != 0) {
+    tasksSection.classList.add("active");
+    footer.classList.add("active");
+  }
+}
+
+window.addEventListener("load", firstLoadRender());
+
+// FILTER TODOS DEPENDING FILTER TYPE
+function filterTasks(filterType, collectElements) {
+  if (filterType == "active") {
+    filterType = false;
+  } else if (filterType == "completed") {
+    filterType = true;
+  }
+
+  let allTasks = document.querySelectorAll(".todos__item");
+  let activeTasks = [];
+  allTasks.forEach((element) => {
+    if (element.querySelector(".todos__toggle").checked === filterType) {
+      collectElements(element, activeTasks);
+    }
+  });
+
+  return activeTasks;
 }
 
 //HIDE TASKS SECTION IF TODOS ARE ABSENT
-function hideTasksSection(){
-        if(!document.querySelector('.todos__item') ){
-                tasksSection.className = 'tasks'
-                footer.className = 'footer'
-        }
+function hideTasksSection() {
+  if (!document.querySelector(".todos__item")) {
+    tasksSection.className = "tasks";
+    footer.className = "footer";
+  }
 }
-
 
 // EVENT ADD TODO ITEM
-newTodo.addEventListener('keydown', function(event){
-	if (event.keyCode === 13 && newTodo.value.trim() ) {
+newTodo.addEventListener("keydown", function (event) {
+  let storage = JSON.parse(localStorage.getItem("todos-list")) || [];
+  if (event.keyCode === 13 && newTodo.value.trim()) {
+    let todoTask = { title: newTodo.value, completed: false, id: uuidv4() };
 
-        let todoTask = {title: newTodo.value, completed : false}
+    storage.push(todoTask);
 
-        storage.push(todoTask)
+    localStorage.setItem("todos-list", JSON.stringify(storage));
 
-        localStorage.setItem('todos-list', JSON.stringify(storage))
+    createNewElement(todoTask.title, todoTask.id, todoTask.completed);
+    changeTodoCount();
 
-        createNewElement ();
-        changeTodoCount()
+    newTodo.value = "";
 
-        newTodo.value = '';
-
-        tasksSection.className = 'tasks active'
-        footer.className = 'footer active'
-
-	}  
+    tasksSection.classList.add("active");
+    footer.classList.add("active");
+  }
 });
 
-
 //CHANGE TODO COUNT
-function changeTodoCount(){
+function changeTodoCount() {
+  function collectElements(element, collector) {
+    collector.push(element);
+  }
 
-        function collectElements(element, collector){
-                collector.push(element) 
-        }
-       
-        let activeTasks = sortTasks('active', collectElements);
+  let activeTasks = filterTasks("active", collectElements);
 
-        todoCount.textContent = activeTasks.length;
+  todoCount.textContent = activeTasks.length;
 
-        let todoCountName = document.querySelector('.todo-count__name')
-        if(activeTasks.length > 1) {
-                todoCountName.textContent = 'items'
-                checkAllBtn.classList.remove('active')
-               
-        } else if(activeTasks.length == 0){
-                checkAllBtn.classList.toggle('active')
-        }
-        else{
-                todoCountName.textContent = 'item'
-                checkAllBtn.classList.remove('active')
-        }
+  let todoCountName = document.querySelector(".todo-count__name");
+  if (activeTasks.length > 1) {
+    todoCountName.textContent = "items";
+    checkAllBtn.classList.remove("active");
+  } else if (activeTasks.length == 0) {
+    checkAllBtn.classList.toggle("active");
+  } else {
+    todoCountName.textContent = "item";
+    checkAllBtn.classList.remove("active");
+  }
 }
-
-
 
 // EVENT DELETE TODO ITEM
-todosList.addEventListener('click', event => {
-        if( event.target.className == "todos__delete"){
-                event.target.closest('.todos__item').remove()
-        }
-        changeTodoCount()
-        hideTasksSection()
-})
+todosList.addEventListener("click", (event) => {
+  if (event.target.className == "todos__delete") {
+    event.target.closest(".todos__item").remove();
+    deleteFromStorage(event.target);
+  }
 
-// INDICATE CHOSEN SORT BUTTON
-let sortBtns = document.querySelector('.sort-btns');
-sortBtns.addEventListener('click', event => {
-        let allSortBtns = sortBtns.querySelectorAll('li');
-        allSortBtns.forEach(item => {
-                item.classList.remove("selected")
-        })
-        // console.log(event.target)
-        event.target.closest('li').classList.add("selected")
-})
+  changeTodoCount();
+  hideTasksSection();
+});
 
+// FUNCTION FOR DELETE ITEM FROM STORAGE
+function deleteFromStorage(element) {
+  let itemId = element.closest("li").getAttribute("item-id");
 
-// SORT TODO LIST, LEFT ACTIVE TASKS
-let sortActiveBtn = document.querySelector('.sort-btns .active') 
-sortActiveBtn.addEventListener('click', event => {
-        sortAllBtn.click()
+  let storage = JSON.parse(localStorage.getItem("todos-list")) || [];
 
-        function hideCheckedItems(element, collector){
-                element.classList.add('hide')
-        }
+  let updatedStorage = storage.filter((item) => item.id != itemId);
 
-        sortTasks('completed', hideCheckedItems);
-})
-
-
-// SORT TODO LIST, LEFT COMPLETED TASKS
-let sortCompletedBtn = document.querySelector('.sort-btns .completed') 
-sortCompletedBtn.addEventListener('click', event => {
-        sortAllBtn.click()
-
-        function hideActiveItems(element, collector){
-                element.classList.add('hide')
-        }
-        
-        sortTasks('active', hideActiveItems);
-})
-
-
-// SORT TODO LIST, LEFT ALL TASKS
-let sortAllBtn = document.querySelector('.sort-btns .all') 
-sortAllBtn.addEventListener('click', event => {
-        let allTasks = document.querySelectorAll('.todos__item')
-        allTasks.forEach(element => {
-                element.classList.remove('hide')
-        })
-})
-
-
-// EVENT SWITCH ALL CHECKBOX TO TRUE
-checkAllBtn.addEventListener('click', event => {
-        checkAllBtn.classList.toggle('active')
-        let allTasks = document.querySelectorAll('.todos__item')
-
-        if([...allTasks].every(item => item.querySelector('.todos__toggle').checked == true)){
-                allTasks.forEach(item => {
-                        item.querySelector('.todos__toggle').checked = false;
-                })
-        } else {
-                allTasks.forEach(item => {
-                        item.querySelector('.todos__toggle').checked = true;
-                })
-                checkAllBtn.classList.toggle('active')
-        }
-        
-        changeTodoCount()
-        sortBySortMode()
-})
-
-// SORT ITEMS AFTER CLICK ON CHECKBOX BY CHOSEN SORT MODE
-todosList.addEventListener('click', event => {
-        if( event.target.className == "todos__toggle"){
-                sortBySortMode()
-        }
-       
-})
-
-// SORT BY CHOSEN SORT MODE
-function sortBySortMode(){
-        let sortType = document.querySelector('.sort-btns .selected')
-        if (sortType.classList.contains('active')){
-                sortActiveBtn.click()
-        } else if(sortType.classList.contains('completed')) {
-                sortCompletedBtn.click()
-        }
+  localStorage.setItem("todos-list", JSON.stringify(updatedStorage));
 }
 
+// INDICATE CHOSEN FILTER BUTTON
+let filterBtns = document.querySelector(".filter-btns");
+filterBtns.addEventListener("click", (event) => {
+  let allSortBtns = filterBtns.querySelectorAll("li");
+  allSortBtns.forEach((item) => {
+    item.classList.remove("selected");
+  });
+  // console.log(event.target)
+  event.target.closest("li").classList.add("selected");
+});
 
+// FILTER TODO LIST, LEFT ACTIVE TASKS
+let filterActiveBtn = document.querySelector(".filter-btns .active");
+filterActiveBtn.addEventListener("click", (event) => {
+  filterAllBtn.click();
 
+  function hideCheckedItems(element, collector) {
+    element.classList.add("hide");
+  }
 
+  filterTasks("completed", hideCheckedItems);
+});
 
+// FILTER TODO LIST, LEFT COMPLETED TASKS
+let filterCompletedBtn = document.querySelector(".filter-btns .completed");
+filterCompletedBtn.addEventListener("click", (event) => {
+  filterAllBtn.click();
 
+  function hideActiveItems(element, collector) {
+    element.classList.add("hide");
+  }
 
+  filterTasks("active", hideActiveItems);
+});
 
+// FILTER TODO LIST, LEFT ALL TASKS
+let filterAllBtn = document.querySelector(".filter-btns .all");
+filterAllBtn.addEventListener("click", (event) => {
+  let allTasks = document.querySelectorAll(".todos__item");
+  allTasks.forEach((element) => {
+    element.classList.remove("hide");
+  });
+});
 
+// EVENT SWITCH ALL CHECKBOX TO CHECKED
+checkAllBtn.addEventListener("click", (event) => {
+  checkAllBtn.classList.toggle("active");
+  let allTasks = document.querySelectorAll(".todos__item");
+  let storage = JSON.parse(localStorage.getItem("todos-list")) || [];
+  let updatedStorage = [];
 
+  if (
+    [...allTasks].every(
+      (item) => item.querySelector(".todos__toggle").checked == true
+    )
+  ) {
+    allTasks.forEach((item) => {
+      item.querySelector(".todos__toggle").checked = false;
+
+      updatedStorage = storage.map((item) => {
+        item.completed = false;
+        return item;
+      });
+    });
+  } else {
+    allTasks.forEach((item) => {
+      item.querySelector(".todos__toggle").checked = true;
+
+      updatedStorage = storage.map((item) => {
+        item.completed = true;
+        return item;
+      });
+    });
+    checkAllBtn.classList.toggle("active");
+  }
+
+  localStorage.setItem("todos-list", JSON.stringify(updatedStorage));
+
+  changeTodoCount();
+  filterBySortMode();
+});
+
+// FILTER ITEMS AFTER CLICK ON CHECKBOX BY CHOSEN FILTER MODE
+todosList.addEventListener("click", (event) => {
+  if (event.target.className == "todos__toggle") {
+    filterBySortMode();
+
+    let storage = JSON.parse(localStorage.getItem("todos-list")) || [];
+
+    let itemId = event.target.closest(".todos__item").getAttribute("item-id");
+
+    let updatedStorage = storage.map((item) => {
+      if (item.id == itemId) {
+        item.completed = !item.completed;
+        return item;
+      } else {
+        return item;
+      }
+    });
+
+    localStorage.setItem("todos-list", JSON.stringify(updatedStorage));
+  }
+});
+
+function switchCompletedFag() {}
+
+// FILTER BY CHOSEN FILTER MODE
+function filterBySortMode() {
+  let filterType = document.querySelector(".filter-btns .selected");
+
+  if (filterType.classList.contains("active")) {
+    filterActiveBtn.click();
+  } else if (filterType.classList.contains("completed")) {
+    filterCompletedBtn.click();
+  }
+}
+
+// EVENT DELETE ALL CHECKED ITEMS
+let deleteChecked = document.querySelector(".delete-checked");
+deleteChecked.addEventListener("click", (event) => {
+  let allTasks = document.querySelectorAll(".todos__item");
+  allTasks.forEach((item) => {
+    if (item.querySelector(".todos__toggle").checked == true) {
+      item.remove();
+    }
+  });
+
+  let storage = JSON.parse(localStorage.getItem("todos-list")) || [];
+
+  let updatedStorage = storage.filter((element) => element.completed == false);
+
+  localStorage.setItem("todos-list", JSON.stringify(updatedStorage));
+  changeTodoCount();
+  hideTasksSection();
+});
